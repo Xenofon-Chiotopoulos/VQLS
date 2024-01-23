5d0a29dfb51dac7661cf561e402cef3b4881d1fe
Xenofon-Chiotopoulos <xenofonchiotopoulos@gmail.com>
2024-01-23 16:44:45 +0100

    fixing conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,6 @@ import sys
 import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('../..'))
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -33,5 +32,5 @@ exclude_patterns = []
 
 html_baseurl = ''
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 html_output = os.path.abspath('../build/html')
