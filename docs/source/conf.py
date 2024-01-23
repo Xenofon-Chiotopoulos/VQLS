2066c39dc310305d1a809b09e11417a5bbba8677
Xenofon-Chiotopoulos <xenofonchiotopoulos@gmail.com>
2024-01-23 16:29:55 +0100

    adding build to path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,9 @@
 import os
 import sys
-import sphinx_rtd_theme  # Add this line
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath('../..'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -19,25 +20,18 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_baseurl = ''
-html_theme = 'alabaster'
-html_static_path = ['_static']
-
-
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx_rtd_theme',  # Add this line
 ]
 
-html_theme = 'sphinx_rtd_theme'  # Add this line
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_baseurl = ''
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_output = os.path.abspath('../build/html')
