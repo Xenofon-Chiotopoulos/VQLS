e75088a148381ecbbd3fed6cf7bebea424a6f684
Xenofon-Chiotopoulos <xenofonchiotopoulos@gmail.com>
2024-01-23 15:30:34 +0100

    adding sphinx doc

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'vqls_docs'
+copyright = '2024, Xenofon Chiotopoulos'
+author = 'Xenofon Chiotopoulos'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+import os
+import sys
+import sphinx_rtd_theme  # Add this line
+
+sys.path.insert(0, os.path.abspath('../src'))
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_rtd_theme',  # Add this line
+]
+
+html_theme = 'sphinx_rtd_theme'  # Add this line
---
+++ docs/source/vqls_utils.rst
@@ -0,0 +1,7 @@
+vqls_utils module
+=================
+
+.. automodule:: vqls_utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
